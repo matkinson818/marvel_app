52df37547928f995e3176951ec7c7c19da05676c
Melveta Atkinson <melvetaatkinson818@gmail.com>
2019-03-09 17:16:53 -0500

    worked on getting image to display

--- src/components/Comics.js
+++ src/components/Comics.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 import axios from 'axios'
-import { url } from 'inspector';
 
 let md5 = require('md5')
 
 class Comics extends Component {
+
     constructor() {
         super();
 
@@ -12,31 +12,35 @@ class Comics extends Component {
         this.api_key = '15d2ff88086bb8aab041f5ea70cc8fbd';
         this.priv_key = 'd567b22a62562345c4382b7f0ea5e442ebfb514d';
         this.hash = md5(this.ts + this.priv_key + this.api_key).toString();
+        this.size = 'portrait_medium'
     }
 
     state = {
-      comics: []
-    }
+        pictures: [],
+      }
 
-    componentDidMount() {
+      componentDidMount() {
         this.getComics();
     }
 
-    getData() {
-        axios.get(`http://gateway.marvel.com/v1/public/comics?apikey=${this.api_key}&hash=${this.hash}&ts=${this.ts}`)
+    async getComics() {
+        axios.get(`http://gateway.marvel.com/v1/public/characters?apikey=${this.api_key}&hash=${this.hash}&ts=${this.ts}`)
         .then(res => {
-          // let characters = res.data.data.results;
-          this.setState({imgs: res.data.data.results.thumbnail.path})
+          // let imgs = res.data.data.results;
+          this.setState({ pictures: res.data.data.results[0].thumbnail });
+          console.log(res)
         })
         .then(err => console.log(err))
-    }
+    } 
 
   render() {
-      let imgs = imgs.map(img => <img className="img-thumbnail" src={imgs.thumbnail} key={img.id}></img>)
+    console.log('hello')
+
     return (
-      <ul>
-        {imgs}
-      </ul>
+        <div>
+            <h1>Hi</h1>
+            <img className="img-thumbnail" src={`${this.state.pictures.path}/portrait_medium.jpg`} />
+        </div>  
     )
   }
 }
